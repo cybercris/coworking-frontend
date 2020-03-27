002246749adc2274532f615ad107f3e57aad132f
cyberCris <cristiano.adlim@gmail.com>
2020-03-27 17:10:40 -0300

    header structure done

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,31 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Top,
+  Logo,
+  CompanyInfo,
+  CompanyData,
+  Avatar,
+  Text,
+} from './styles';
 
 export default function Header() {
-  return <h1>Header</h1>;
+  return (
+    <Container>
+      <Top>
+        <Logo>Coworking</Logo>
+        <IconDoorOut />
+      </Top>
+      <CompanyInfo>
+        <Avatar />
+        <CompanyData>
+          <Text>Concrete Solutions</Text>
+          <Text>Rua benfica, N 90</Text>
+          <Text>concrete@gmail.com</Text>
+          <Text>(81)</Text>
+        </CompanyData>
+      </CompanyInfo>
+    </Container>
+  );
 }
---
+++ src/components/Header/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+`;
+
+export const Top = styled.div``;
+
+export const CompanyInfo = styled.div``;
