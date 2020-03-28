db804fc73db78cc4af8f83209522a445d107fbb1
cyberCris <cristiano.adlim@gmail.com>
2020-03-28 12:59:43 -0300

    header done

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { GoSignOut } from 'react-icons/go';
+import { FaUserCircle } from 'react-icons/fa';
 
 import {
   Container,
@@ -15,15 +17,17 @@ export default function Header() {
     <Container>
       <Top>
         <Logo>Coworking</Logo>
-        <IconDoorOut />
+        <GoSignOut size={30} color="#FFF" />
       </Top>
       <CompanyInfo>
-        <Avatar />
+        <Avatar>
+          <FaUserCircle size={130} />
+        </Avatar>
         <CompanyData>
           <Text>Concrete Solutions</Text>
           <Text>Rua benfica, N 90</Text>
           <Text>concrete@gmail.com</Text>
-          <Text>(81)</Text>
+          <Text>(81) 3236-4534</Text>
         </CompanyData>
       </CompanyInfo>
     </Container>
